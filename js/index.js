a3ce36d7052efd39cfa6c8b39581f8a196c1cf36
Akshat Kumar <akshatkumar4433@gmail.com>
2022-01-16 13:22:29 -0500

    Julis

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
+<<<<<<< HEAD
 console.log('hello')
+=======
+Vue.component('key',{
+   props:['character'],
+   template: `
+    <button>{{character}}</button>
+   `,
+})
+
+new Vue({el:'#app'})
+>>>>>>> first commit

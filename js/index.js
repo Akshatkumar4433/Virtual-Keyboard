b37fc433da0f4d7108e2fc8e9c542ab7e3b7974b
Akshat Kumar <akshatkumar4433@gmail.com>
2022-01-17 23:23:51 -0500

    second commit

--- js/index.js
+++ js/index.js
@@ -1,12 +1,58 @@
-<<<<<<< HEAD
-console.log('hello')
-=======
+const english = ['q','w','e','r','t', 'y',
+          'u','i','o', 'p','a','s',
+          'd','f','g','h','j','k',
+           'i', 'z','x',
+           'c','v','b','n','m',];
+
 Vue.component('key',{
-   props:['character'],
+   props:['keyInfo'],
    template: `
-    <button>{{character}}</button>
+    <button v-on:click = 'keyInfo.task(keyInfo.character)'>{{keyInfo.character}}</button>
    `,
 })
 
-new Vue({el:'#app'})
->>>>>>> first commit
+data = {
+     alphabetList: alphabetListToDataset(english)
+}
+
+new Vue({el:'#app',data})
+
+function alphabetListToDataset(alphabetList) {
+     let Dataset = [];
+     let KeyboardSize = 31;
+     for(let i = 0, j = 0; i<KeyboardSize; i++) {
+       let element = {id:i+1,
+                      character:'',
+                      task:task};
+       if (i == 19) {
+          element.character = 'shift';
+       }
+       else if (i == 27) {
+          element.character = 'Backspace';
+       }
+       else if (i == 28) {
+          element.character = '123';
+       }
+       else if (i == 29) {
+         element.character = 'space';
+       }
+       else if (i == 30) {
+         element.character = 'return';
+       }
+       else {
+         element.character = alphabetList[j];
+         j++;
+       }
+       Dataset.push(element);
+     }
+     return Dataset;
+}
+
+
+/***************
+These are expriemnt
+*******/
+
+function task(button) {
+  console.log(button)
+}
